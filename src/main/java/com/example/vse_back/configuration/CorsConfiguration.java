126f7c75f6fc00287653719ca95bd55b11e75a35
barenike <m-s-ka@mail.ru>
2023-05-24 17:45:02 +0500

    CORS config changes

--- src/main/java/com/example/vse_back/configuration/CorsConfiguration.java
+++ src/main/java/com/example/vse_back/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.vse_back.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+    @Value("${host.frontend}")
+    private String hostFrontend;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        // Change to static IP (get from application.yaml variable)
-                        .allowedOriginPatterns("*")
-                        //.allowedOrigins("http://localhost:8080")
+                        .allowedOrigins("http://localhost:8080", hostFrontend)
                         .allowedHeaders("*")
                         .allowedMethods("*")
                         .allowCredentials(true);
